566499be369f4ab7b640c354a1341f19cb1c7cb6
Amirmohammad <52940947+amirrahmani76@users.noreply.github.com>
2022-03-07 03:51:42 +0330

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBemmfIItN5qAgFEFvOS4neverWvxDf1lY',
-  authDomain: 'ama2-863d9.firebaseapp.com',
-  projectId: 'ama2-863d9',
-  storageBucket: 'ama2-863d9.appspot.com',
-  messagingSenderId: '367902818172',
-  appId: '1:367902818172:web:488181011b70ee1537aef9',
+  apiKey: '**********',
+  authDomain: '***********',
+  projectId: '*********',
+  storageBucket: '*************',
+  messagingSenderId: '*************',
+  appId: '**************',
 };
